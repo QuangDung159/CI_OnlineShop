03f74b532c6c02e21263cb47696af710bc287d03
DungLQ159 <quangdunglu159@gmail.com>
2018-12-02 13:16:00 +0700

    - Add MY_Controller

---
+++ application/controllers/admin/Login.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DungLQ7
+ * Date: 12/2/2018
+ * Time: 12:46 PM
+ */
+
+class Login extends MY_Controller
+{
+    function index()
+    {
+        echo "Login";
+    }
+}
--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -5,3 +5,35 @@
  * Date: 12/2/2018
  * Time: 12:41 PM
  */
+
+class MY_Controller extends CI_Controller
+{
+    function __construct()
+    {
+        // Kế thừa từ CI_Controller
+        parent::__construct();
+        $controller = $this->uri->segment(1);
+
+        // Phân luồng đăng nhập
+        switch ($controller) {
+            case "admin":
+                {
+                    // Xử lý các dữ liệu khi truy cập vào trang admin
+                    $this->check_login();
+                    break;
+                }
+            default :
+                {
+                    // Xử lý dữ liệu ở trang ngoài
+                }
+        }
+    }
+
+    /*
+     * Kiểm tra trạng thái đăng nhập của admin
+     */
+    private function check_login()
+    {
+
+    }
+}

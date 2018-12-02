13d805ad6f45eb5109d3f64790ddca6531dd3f59
DungLQ159 <quangdunglu159@gmail.com>
2018-12-02 14:08:49 +0700

    - Add Login.php

---
+++ application/views/admin/login/index.php
@@ -0,0 +1,65 @@
+<html>
+
+<head>
+
+</head>
+
+<body class="nobg loginPage">
+
+<!-- Top fixed navigation -->
+<div class="topNav">
+    <div class="wrapper">
+        <div class="userNav">
+            <ul>
+                <li><a href="#" title=""><img src="images/icons/topnav/mainWebsite.png"
+                                              alt=""/><span>Main website</span></a></li>
+                <li><a href="#" title=""><img src="images/icons/topnav/profile.png"
+                                              alt=""/><span>Contact admin</span></a></li>
+                <li><a href="#" title=""><img src="images/icons/topnav/messages.png" alt=""/><span>Support</span></a>
+                </li>
+                <li><a href="login.html" title=""><img src="images/icons/topnav/settings.png"
+                                                       alt=""/><span>Settings</span></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+
+<!-- Main content wrapper -->
+<div class="loginWrapper">
+    <div class="loginLogo"><img src="images/loginLogo.png" alt=""/></div>
+    <div class="widget">
+        <div class="title"><img src="images/icons/dark/files.png" alt="" class="titleIcon"/><h6>Login panel</h6></div>
+        <form action="index.html" id="validate" class="form">
+            <fieldset>
+                <div class="formRow">
+                    <label for="login">Username:</label>
+                    <div class="loginInput"><input type="text" name="login" class="validate[required]" id="login"/>
+                    </div>
+                </div>
+
+                <div class="formRow">
+                    <label for="pass">Password:</label>
+                    <div class="loginInput"><input type="password" name="password" class="validate[required]"
+                                                   id="pass"/></div>
+                </div>
+
+                <div class="loginControl">
+                    <div class="rememberMe"><input type="checkbox" id="remMe" name="remMe"/><label for="remMe">Remember
+                            me</label></div>
+                    <input type="submit" value="Log me in" class="dredB logMeIn"/>
+                </div>
+            </fieldset>
+        </form>
+    </div>
+</div>
+
+<!-- Footer line -->
+<div id="footer">
+    <div class="wrapper">As usually all rights reserved. And as usually brought to you by <a
+                href="http://themeforest.net/user/Kopyov?ref=kopyov" title="">Eugene Kopyov</a></div>
+</div>
+
+
+</body>
+</html>
